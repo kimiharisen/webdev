7714a0652fd388d3094562f68e6919730d9e7764
kimiharisen <146687687+kimiharisen@users.noreply.github.com>
2023-10-02 10:59:26 +0700

    WebDevWeek4
    week 4

---
+++ example-app/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+
+@section('layout_content')
+
+<p>jdsvjdsnvjdsjvndsjvcnjs</p>
+
+<p>Contact: willycahyono@gmail.com</p>
+
+<x-mybutton> Contact Us </x-mybutton>
+
+@endsection
+    
+
+ 
---
+++ example-app/resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.template')
+
+@section('layout_content')
+
+   <h2>Please contact us</h2>
+    <table>
+        <tr>
+            <td>Phone</td>
+            <td>0987654321</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>office@company.com</td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td>Main Street 47, Jakarta</td>
+        </tr>
+    </table>
+@endsection
